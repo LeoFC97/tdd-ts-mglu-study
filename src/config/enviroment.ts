395a58a68bf5ce80350b857b0ded24e59fae2b8a
LeoFC97 <leonardofernancesc@gmail.com>
2021-06-18 16:11:36 -0300

    fix: add correct default strings

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -1,6 +1,6 @@
 export const mysql = {
-  host: process.env.MYSQL_HOST,
-  username: process.env.MYSQL_USERNAME || 'admin',
+  host: process.env.MYSQL_HOST || 'localhost',
+  username: process.env.MYSQL_USERNAME || 'root',
   password: process.env.MYSQL_PASSWORD || 'root',
   database: process.env.MYSQL_DATABASE || 'store',
   port: process.env.MYSQL_PORT || '3306',
