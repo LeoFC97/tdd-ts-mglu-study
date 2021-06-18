c7ce32683657964f97c451e7301eab6d4c24e704
LeoFC97 <leonardofernancesc@gmail.com>
2021-06-18 17:46:20 -0300

    fix: remove database test

--- src/app/api/http/controllers/health.test.ts
+++ src/app/api/http/controllers/health.test.ts
@@ -1,17 +1,8 @@
 /* eslint-disable max-len */
 import supertest from 'supertest';
 import startExpressServer from '../../../drivers/http/server';
-import MysqlDBManager from '../../../drivers/mysql/mysql-manager';
-import { mysql } from '../../../../config/enviroment';
 
 describe('Test health response', () => {
-  beforeAll(async () => {
-    mysql.database = 'mockedDatabase';
-    await MysqlDBManager.initialize();
-  });
-  afterAll(async () => {
-    await MysqlDBManager.closeConnection();
-  });
   test('Should body with datetime and database status on body', async () => {
     const apiMocked = await supertest(startExpressServer());
     const result = await apiMocked.get('/health');
