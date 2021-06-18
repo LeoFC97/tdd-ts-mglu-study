4ce46964255a79be0156099eae3db1789d096d22
LeoFC97 <leonardofernancesc@gmail.com>
2021-06-18 15:57:34 -0300

    feat: add mysqlDbManager core

---
+++ src/app/drivers/mysql/mysql-manager.ts
@@ -0,0 +1,42 @@
+import {
+  createConnection, Connection, getConnection,
+} from 'typeorm';
+
+import { mysql } from '../../../config/enviroment';
+
+export default class MysqlDBManager {
+  static async initialize(): Promise <Connection> {
+    try {
+      console.log(mysql);
+      const connection = await createConnection({
+        type: 'mysql',
+        host: mysql.host,
+        port: Number(mysql.port),
+        username: mysql.username,
+        password: mysql.password,
+        database: mysql.database,
+        entities: [
+          '**/*-entity.ts',
+        ],
+      });
+      console.log('Connected to Mysql Database');
+      return connection;
+    } catch ({ message }) {
+      console.log(message);
+      return message;
+    }
+  }
+  static async checkConnection(): Promise <boolean> {
+    const connecion = await getConnection().isConnected;
+    return connecion;
+  }
+  static async closeConnection(): Promise <boolean> {
+    try {
+      await getConnection().close;
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
