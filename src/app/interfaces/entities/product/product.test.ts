182667e75abd1087997ab0233c23bd24f3b77f70
LeoFC97 <leonardofernancesc@gmail.com>
2021-06-19 10:41:03 -0300

    fix: fix mock name

--- src/app/interfaces/entities/product/product.test.ts
+++ src/app/interfaces/entities/product/product.test.ts
@@ -2,17 +2,17 @@ import Product from './product';
 
 describe('Enruse types of Product interface', () => {
   test('Should make sure Product have the correct types', async () => {
-    const mockedClient: Product = {
+    const mockedProduct: Product = {
       code: '1',
       name: 'pencil sharpener',
       color: 'blue',
       size: 'Big',
       value: 12.40,
     };
-    expect(typeof mockedClient.code).toBe('string');
-    expect(typeof mockedClient.name).toBe('string');
-    expect(typeof mockedClient.color).toBe('string');
-    expect(typeof mockedClient.size).toBe('string');
-    expect(typeof mockedClient.value).toBe('number');
+    expect(typeof mockedProduct.code).toBe('string');
+    expect(typeof mockedProduct.name).toBe('string');
+    expect(typeof mockedProduct.color).toBe('string');
+    expect(typeof mockedProduct.size).toBe('string');
+    expect(typeof mockedProduct.value).toBe('number');
   });
 });
