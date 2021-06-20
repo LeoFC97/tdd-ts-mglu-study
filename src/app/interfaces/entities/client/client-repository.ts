130092334e039dc0a9ef0fdef4ab44b5f0d280c9
LeoFC97 <leonardo.fc2@outlook.com>
2021-06-19 23:35:45 -0300

    feat: add implementation of getAllClients

--- src/app/interfaces/entities/client/client-repository.ts
+++ src/app/interfaces/entities/client/client-repository.ts
@@ -2,7 +2,7 @@ import { InsertResult } from 'typeorm';
 import Client from './client';
 
 export default interface ClientRepository {
-  // getAllClients(): Promise<Client[]>;
+  getAllClients(): Promise<Client[]>;
   getClientById(id: number): Promise<Client>;
   createClient(clientToBeAdded: Client): Promise<boolean>;
   // updateClientById(id: number): Promise<Client>;
